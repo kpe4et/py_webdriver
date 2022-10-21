fac18d093f4f7fb3668df8b17a0d7c8be85211bf
Петров Игорь Владимирович <petrov.igor@euroauto.acc>
2022-10-21 15:47:13 +0300

    product page tests

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,10 @@ class LoginPageLocators():
     LOGIN_URL = "login"
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form [type='submit']")
+    PRODUCT_NAME_H1 = (By.CSS_SELECTOR, "h1")
+    PRODUCT_NAME_NOTIFY = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > .alertinner strong")
+    PRODUCT_PRICE_DIV = (By.CSS_SELECTOR, ".product_main p.price_color")
+    PRODUCT_PRICE_NOTIFY = (By.CSS_SELECTOR, "#messages > div:nth-child(3) p:nth-child(1) strong")
---
+++ pages/product_page.py
@@ -0,0 +1,17 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_to_cart(self):
+        add_to_cart_button = self.browser.find_element(*ProductPageLocators.ADD_TO_CART_BUTTON)
+        add_to_cart_button.click()
+
+    def check_product_name_in_notification(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_H1).text
+        product_in_cart_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_NOTIFY).text
+        assert product_name == product_in_cart_name, "Wrong product in cart"
+
+    def check_product_in_cart_price(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_DIV).text
+        product_price_in_cart = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_NOTIFY).text
+        assert product_price == product_price_in_cart, "Wrong price in cart"
