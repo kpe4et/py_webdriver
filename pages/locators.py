95b3c667925ea28f610e11f16e0fba45e9fcb572
kpe4et <kpe4ets@gmail.com>
2022-10-21 16:51:58 +0300

    negative checks

--- pages/locators.py
+++ pages/locators.py
@@ -14,3 +14,4 @@ class ProductPageLocators():
     PRODUCT_NAME_NOTIFY = (By.CSS_SELECTOR, "#messages > div:nth-child(1) > .alertinner strong")
     PRODUCT_PRICE_DIV = (By.CSS_SELECTOR, ".product_main p.price_color")
     PRODUCT_PRICE_NOTIFY = (By.CSS_SELECTOR, "#messages > div:nth-child(3) p:nth-child(1) strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "p.success")
--- pages/product_page.py
+++ pages/product_page.py
@@ -15,3 +15,11 @@ class ProductPage(BasePage):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_DIV).text
         product_price_in_cart = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_NOTIFY).text
         assert product_price == product_price_in_cart, "Wrong price in cart"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+                "Success message is presented, but shouldn't be"
+
+    def should_dissapear(self):
+        assert self.is_dissapeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+                "Success message is presented, but had to dissapear"
